df262fb66844808f87b1e535095b5be26eae8cb5
McClellan-Micah <mcclellan.micah.6@gmail.com>
2020-06-19 09:09:47 -0700

    ask user their desired password type

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,23 @@
 var generateBtn = document.querySelector("#generate");
 function generatePassword() {
     //create empty array for password
+    var password = [];
 
     //create random variables to obtain user input
+    var confirmLength = window.prompt("Choose between 8 and 128 characters for your password.");
+        confirmLength = parseInt(confirmLength);
+
+    var confirmLower = window.confirm("Would you like to include lowercase letters in your password?");
+        console.log("Lowercase: " + confirmLower);
+
+    var confirmUpper = window.confirm("Would you like to include uppercase letters in your password?");
+        console.log("Uppercase: " + confirmUpper);
+
+    var confirmNumeric = window.confirm("Would you like to include numbers in your password?");
+        console.log("Numbers: " + confirmNumeric);
+
+    var confirmSymbol = window.confirm("Would you like to include special characters in your password?");
+        console.log("Symbols: " + confirmSymbol);
 
     //return password
 }
