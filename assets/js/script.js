a01b2a1ef78d1f7d74b285687bb69d2c816ce294
McClellan-Micah <mcclellan.micah.6@gmail.com>
2020-06-19 09:15:59 -0700

    create random character functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ function generatePassword() {
     //create empty array for password
     var password = [];
 
-    //create random variables to obtain user input
+    //create if statements to grab random functions
     var confirmLength = window.prompt("Choose between 8 and 128 characters for your password.");
         confirmLength = parseInt(confirmLength);
 
@@ -27,6 +27,26 @@ function generatePassword() {
 }
 
 //create functions that contain arrays for randomized characters
+function getRandomLower() {
+    const getRandomLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    return getRandomLower[Math.floor(Math.random() * getRandomLower.length)];
+}
+
+function getRandomUpper() {
+    const getRandomUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    return getRandomUpper[Math.floor(Math.random() * getRandomUpper.length)];
+}
+
+function getRandomNumber() {
+    const getRandomNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    return getRandomNumber[Math.floor(Math.random() * getRandomNumber.length)];
+}
+
+function getRandomSymbol() {
+    const getRandomSymbol = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", " ", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^","_", "`", "{", "|", "}", "~"];
+    return getRandomSymbol[Math.floor(Math.random() * getRandomSymbol.length)]; 
+}
+
 
 // Write password to the #password input
 function writePassword() {
