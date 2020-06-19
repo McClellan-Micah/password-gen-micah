3a0be4aa3c1d5cdd1d9b696c14f0b4286b6932c5
McClellan-Micah <mcclellan.micah.6@gmail.com>
2020-06-19 09:03:12 -0700

    pseudocode js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,14 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 function generatePassword() {
+    //create empty array for password
+
+    //create random variables to obtain user input
+
+    //return password
 }
+
+//create functions that contain arrays for randomized characters
 
 // Write password to the #password input
 function writePassword() {
